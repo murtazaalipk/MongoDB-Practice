dd51b0a5ebea8fe1ce93ae0409e5fa1fbb734b2f
Syed Murtaza Ali <125219980+murtazaalipk@users.noreply.github.com>
2024-02-03 20:22:33 +0500

    Update server.js

--- form-atlas/server.js
+++ form-atlas/server.js
@@ -5,7 +5,17 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb+srv://murtazaali:hXOVq466dFX8hq2s@cluster0.j1kwwam.mongodb.net/Practice")
+mongoose.connect("mongodb+srv://murtazaali:hXOVq466dFX8hq2s@cluster0.j1kwwam.mongodb.net/Practice" , {useNewUrlParser: true}, { useUnifiedTopology: true });
+
+// Create a schema
+
+const formSchema = {
+    name: String,
+    email: String,
+    password: String
+};
+
+const Form = mongoose.model("Form", formSchema);
 
 app.get('/', (req, res) => {
     res.send('Express server running');
