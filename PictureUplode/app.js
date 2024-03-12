dc845b5cd1bc1f14a2b3619a43ad7764783792fd
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2024-03-12 16:25:03 +0500

    express setup

--- PictureUplode/app.js
+++ PictureUplode/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
 
 /* app.get('/', (req, res) => {
   res.send('Hello World')
