5550f6e7728d76bc664792868ea34b04d3f77eef
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2024-03-13 23:30:18 +0500

    update app.js

--- PictureUplode/app.js
+++ PictureUplode/app.js
@@ -3,9 +3,15 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const multer = require('multer');
 
+const ImageModel = require("./image.model")
+
+
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb+srv://murtazaali:hXOVq466dFX8hq2s@cluster0.j1kwwam.mongodb.net/ImageDB" , {useNewUrlParser: true}, { useUnifiedTopology: true });
 
 
 /* app.get('/', (req, res) => {
