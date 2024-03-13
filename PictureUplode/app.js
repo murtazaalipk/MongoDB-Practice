ab6d883c96ea934882b27e78fdb3dc1c6bd0e613
Syed Murtaza Ali <syedmurtazaaliofficial@gmail.com>
2024-03-13 23:57:41 +0500

    update app.js

--- PictureUplode/app.js
+++ PictureUplode/app.js
@@ -18,8 +18,39 @@ mongoose.connect("mongodb+srv://murtazaali:hXOVq466dFX8hq2s@cluster0.j1kwwam.mon
   res.send('Hello World')
 })
  */
+
+const Storage = multer.diskStorage({
+  destination: 'uplode',
+  filename: (req,file,cb) => {
+    cb(null, file.originalname)
+  },
+});
+
+const uplode = multer ({
+  storage:Storage
+}).single('testImage')
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/uplode',(req, res) =>{
+    uplode(req,res,(err)=>{
+      if(err){
+        console.log(err)
+      }
+      else{
+        const newImage = new ImageModel({
+        name: req.body.name:
+        image:{
+          data:req.file.filename,
+          contentType: "image/png"
+        }
+        })
+        newImage.save()
+        .then(()=> res.send("succeessfully uplode")).catch(err=>console.log(err))
+      }
+    })
 })
 
 app.listen(port, () => {
